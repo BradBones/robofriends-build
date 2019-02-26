820236c0402cf20d6b347ee44e29eeddc412728b
BradleyBones <38480426+BradBones@users.noreply.github.com>
2019-02-26 12:39:05 -0800

    Add files via upload

---
+++ precache-manifest.94eb68e5a7a9b4a4300584e4c24c29d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-build/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fd1bcca5a0eb55325ff2",
+    "url": "/robofriends-build/static/js/runtime~main.fd1bcca5.js"
+  },
+  {
+    "revision": "65bbe5f008c7757546b7",
+    "url": "/robofriends-build/static/js/main.65bbe5f0.chunk.js"
+  },
+  {
+    "revision": "e0c1a02ddafe6e1b440c",
+    "url": "/robofriends-build/static/js/2.e0c1a02d.chunk.js"
+  },
+  {
+    "revision": "65bbe5f008c7757546b7",
+    "url": "/robofriends-build/static/css/main.c85259e6.chunk.css"
+  },
+  {
+    "revision": "e0c1a02ddafe6e1b440c",
+    "url": "/robofriends-build/static/css/2.765371e6.chunk.css"
+  },
+  {
+    "revision": "072ed57b30168d06cd74e317a30647ac",
+    "url": "/robofriends-build/index.html"
+  }
+];
